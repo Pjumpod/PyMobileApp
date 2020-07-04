00316f2ef6eb6d38d6c1f000e797fe2a7d4eddf7
Jumpod Plekhongthu <pjumpod@gmail.com>
2020-07-05 01:22:39 +0700

    Modify the space vs tab in python.

--- calculator.py
+++ calculator.py
@@ -38,29 +38,34 @@ class MainApp(App):
 
         return main_layout
 
-def on_button_press(self, instance):
-    current = self.solution.text
-    button_text = instance.text
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
 
-    if button_text == "C":
-        # Clear the solution widget
-        self.solution.text = ""
-    else:
-        if current and (
-            self.last_was_operator and button_text in self.operators):
-            # Don't add two operators right after each other
-            return
-        elif current == "" and button_text in self.operators:
-            # First character cannot be an operator
-            return
+        if button_text == "C":
+            # Clear the solution widget
+            self.solution.text = ""
         else:
-            new_text = current + button_text
-            self.solution.text = new_text
-    self.last_button = button_text
-    self.last_was_operator = self.last_button in self.operators
+            if current and (
+                self.last_was_operator and button_text in self.operators):
+                # Don't add two operators right after each other
+                return
+            elif current == "" and button_text in self.operators:
+                # First character cannot be an operator
+                return
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+        self.last_button = button_text
+        self.last_was_operator = self.last_button in self.operators
 
-def on_solution(self, instance):
-    text = self.solution.text
-    if text:
-        solution = str(eval(self.solution.text))
-        self.solution.text = solution
+    def on_solution(self, instance):
+        text = self.solution.text
+        if text:
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
